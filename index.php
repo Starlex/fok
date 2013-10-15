c95f067acb8829a6fbc44f5394e5d3e3924bca8c
Alex <leyvatein@mail.ru>
2013-10-15 16:34:29 +0600

    added minor changes

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 require_once "./block/header.php";
-if(""===$_GET['page']){
+if(empty($_GET) or ""===$_GET['page']){
 	require_once "./pages/main.php";
 }
-$var = "asdf";
-print_r($_GET);
-echo "<pre>";
-print_r($GLOBALS);
 require_once "./block/footer.php";
 ?>
--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,1 @@
-<?php
-echo "sdrgh";
-?>
+<a href="asdf">asdfsdf</a>
