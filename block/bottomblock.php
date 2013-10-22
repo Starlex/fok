163dc1800952df31d4169ac27cafbdced1f60582
Alex <leyvatein@mail.ru>
2013-10-22 17:52:35 +0600

    started botom menu

--- block/bottomblock.php
+++ block/bottomblock.php
@@ -4,12 +4,7 @@
 			<li><a href='../'>Домашняя страница</a></li>
 			<li><a href='about-us/'>О нас</a></li>
 			<li><a href='our-location/'>Наши координаты</a></li>
-			<li><a href='our-services/'>Наши услуги</a>
-				<!-- <ul>
-					<li><a href="#">asdfasdf</a></li>
-					<li><a href="#">asdfasdf</a></li>
-				</ul> -->
-			</li>
+			<li><a href='our-services/'>Наши услуги</a></li>
 			<li><a href='our-rates/'>Наши тарифы</a></li>
 			<li><a href='news/'>Новости</a></li>
 		</ul>
